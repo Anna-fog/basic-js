1758ae349307d63c70d8ee1a7fffa7e7ea3f4807
Anna-fog <anna49.fog@yandex.ru>
2021-01-26 22:14:04 +0300

    feat: implement 'hanoi-towel' task solution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  const turns = Math.pow(2, disksNumber) - 1;
+  const secondsToOneTurn = 3600 / turnsSpeed;
+  const allSeconds = Math.floor(secondsToOneTurn * turns);
+  return {'turns': turns, 'seconds': allSeconds};
 };
