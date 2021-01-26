be47ab5545b2c5ac37a71d78c30e8f03a5c82b84
Anna-fog <anna49.fog@yandex.ru>
2021-01-26 16:40:01 +0300

    feat: implement three task solutions

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || members.length === 0 || !Array.isArray(members)) return false;
+
+  let firstLetters = [];
+
+  members.map(item => {
+    if (typeof item !== "string") return '';
+    firstLetters.push(item.trim().slice(0, 1).toUpperCase());
+  });
+
+  return firstLetters.sort().join('');
+
 };
