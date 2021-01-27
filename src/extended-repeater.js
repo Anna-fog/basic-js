b981571d22708e9e5b7d8812edddd3f9cce5fc1b
Anna-fog <anna49.fog@yandex.ru>
2021-01-27 19:07:52 +0300

    feat: implement tasks solutions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const {repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|'} = options;
+  if (str && typeof str !== "string") str.toString();
+  if (addition && typeof addition !== "string") addition.toString();
+
+  let resStr = '';
+
+  for (let i = 0; i < repeatTimes; i++) {
+    resStr += str + (addition + additionSeparator).repeat(additionRepeatTimes - 1) + addition;
+    if (i !== repeatTimes - 1) {
+      resStr += separator;
+    }
+  }
+  return resStr;
 };
   
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (typeof arr !== "object") throw new Error('Not an Array!');
+  let newArr = [];
+
+  arr.map((item, i) => {
+    if (item === '--double-next') {
+      newArr.push(i + 1);
+    } else if (item === '--double-prev') {
+      newArr.push(i);
+    } else newArr.push(item);
+  });
+
+  if (newArr.includes('--discard-next') || newArr.includes('--discard-prev')) {
+    newArr = arr.filter((item, i) => {
+      if (arr.includes('--discard-next')) {
+        return (item !== '--discard-next' && i !== arr.indexOf('--discard-next') + 1);
+      } else if (arr.includes('--discard-prev')) {
+        return (item !== '--discard-prev' && i !== arr.indexOf('--discard-prev') - 1);
+      }
+    });
+  }
+  return newArr;
 };
